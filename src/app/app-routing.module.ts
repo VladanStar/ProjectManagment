fb0b6c8db545be729e1840f5b7f4bc66a8598d8c
VladanStar <vladancupric@gmail.com>
2023-03-17 17:22:01 +0100

    Add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './component/login/login.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: '', redirectTo:'file-upload', pathMatch:'full'},
+  {path: 'login', component : LoginComponent},
+  {path: 'dashboard', component : DashboardComponent},
+//  {path: 'register', component : RegisterComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../../environments/environment';
-import { provideAuth,getAuth } from '@angular/fire/auth';
-import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 import{AngularFireModule}from"@angular/fire/compat";
 import { FormsModule } from '@angular/forms';
 import { LoginComponent } from './component/login/login.component';
 import { RegisterComponent } from './component/register/register.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    DashboardComponent
   ],
   imports: [
+
     BrowserModule,
     FormsModule,
     AppRoutingModule,
